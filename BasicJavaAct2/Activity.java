eaca18b73425bf4a8b6164275d489a250bff395b
Robert Joshua S. Zulueta <rjszulueta@headoffice.corp.bpi.com.ph>
2025-12-02 16:56:54 +0800

    initial commit, module 1 Activities

---
+++ BasicJavaAct2/Activity.java
@@ -0,0 +1,18 @@
+package act;
+
+import java.util.Scanner;
+
+class Activity {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        String age = sc.next();
+        int ageInt = Integer.parseInt(age);
+        double ageDouble = Double.parseDouble(age);
+
+        System.out.println("Your Age as int, " + ageInt);
+        System.out.println("Your Age as Double, " + ageDouble);
+
+        sc.close();
+    }
+}
